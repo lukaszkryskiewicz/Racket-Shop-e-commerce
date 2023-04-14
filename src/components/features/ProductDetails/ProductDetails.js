9ed660dfaa75daa937d4c51a23937a344b02ecb4
Lukasz Kryskiewicz <lukasz.kryskiewicz@gmail.com>
2023-04-14 13:44:40 +0200

    Add feature to ProductDetial PhotoSection and add styles to InfoSection

--- src/components/features/ProductDetails/ProductDetails.js
+++ src/components/features/ProductDetails/ProductDetails.js
@@ -2,11 +2,27 @@ import clsx from 'clsx';
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './ProductDetails.module.scss';
+import { useSelector } from 'react-redux';
+import { getProductById } from '../../../redux/productsRedux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import { Link } from 'react-router-dom';
+import Button from '../../common/Button/Button';
 
 const ProductDetails = () => {
   const { productId } = useParams();
-  const [fadeImage, setFadeImage] = useState(false);
-  const [fadeSlider, setFadeSlider] = useState(true);
+  const [fadeImage, setFadeImage] = useState(true);
+  const [activeImage, setActiveImage] = useState(0);
+  const product = useSelector(state => getProductById(state, productId))
+  const pictureNumber = 4;
+  const handlePhotoChange = (number) => {
+    setFadeImage(false);
+    setTimeout(() => {
+      setActiveImage(number);
+      setFadeImage(true);
+    }, 400);
+  };
 
   return (
     <div className={styles.root}>
@@ -19,58 +35,80 @@ const ProductDetails = () => {
                 styles.photo,
                 fadeImage ? styles.fadeIn : styles.fadeOut
               )}
-            >
-              <img src='/images/racket/badminton/badminton-racket-1.webp' />
+            > <p className={styles.photoOverlay}>Picture {activeImage}</p>
+              <img alt={product.name} src={product.source} />
             </div>
             <div className={clsx('row g-0 m-2 justify-content-between', styles.slider)}>
               <a
                 className={'col-1 ' + styles.arrowButton}
-                /*      onClick={() =>
-                     handlePageChange(activePage === 0 ? pagesCount - 1 : activePage - 1)
-                   } */
+                onClick={() =>
+                  handlePhotoChange(activeImage === 0 ? pictureNumber - 1 : activeImage - 1)}
               >
                 &#60;
               </a>
               <div
-                className={clsx(
-                  'col mx-3',
-                  fadeSlider ? styles.fadeIn : styles.fadeOut
-                )}
-              >
-                <div className={clsx('row')}>
-                  {/*      {productsToDisplay
-                    .slice(activePage * columns, (activePage + 1) * columns)
-                    .map(product => ( */}
-                  <div
-                    /* key={product.name} */
-                    className={clsx('col-lg-2 col-md-4 col-3 px-1', styles.thumbnail)}
-                    /* onClick={() => handleProductChange(product)} */
-                  >
-                    <img
-                    /*    alt={product.name}
-                       src={product.source} */
-                    /* className={
-                      product.id === activeThumbnail.id ? styles.active : null
-                    } */
-                    />
-                  </div>
-                  {/*    ))} */}
+                className={clsx('col mx-3')}>
+                <div className={clsx('row', styles.thumbnailsContainer)}>
+                  {[...Array(pictureNumber)].map((item, i) => (
+                    <div
+                      key={i}
+                      className={clsx('col-lg-2 col-md-4 col-3 px-1', styles.thumbnail)}
+                      onClick={() => handlePhotoChange(i)}
+                    >
+                      <img
+                        alt={product.name}
+                        src={product.source}
+                        className={
+                          i === activeImage ? styles.active : null
+                        }
+                      />
+                    </div>
+                  ))}
                 </div>
               </div>
               <a
                 className={clsx('col-1', styles.arrowButton)}
-                /*       onClick={() =>
-                      handlePageChange(activePage + 1 >= pagesCount ? 0 : activePage + 1)
-                    } */
+                onClick={() => handlePhotoChange(activeImage === pictureNumber - 1 ? 0 : activeImage + 1)}
               >
                 <span>&#62;</span>
               </a>
             </div>
           </div>
-          <div className={clsx('col-7', styles.infoSection)}>test2</div>
+          <div className={clsx('col-7', styles.infoSection)}>
+            <div className={clsx('row', styles.headerRow)}>
+              <div className={styles.title}>
+                <h1>{product.name}</h1>
+              </div>
+              <div className={styles.review}>
+                {[1, 2, 3, 4, 5].map(i => (
+                  <span key={i} className={styles.stars}>
+                    {i <= product.stars ? (
+                      <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+                    ) : (
+                      <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+                    )}
+                  </span>
+                ))}
+                <p className={styles.reviewCall}>(0 reviews)</p>
+                <Link to={`/product/${product.id}/review`}>
+                  <Button variant='outline'>Add your review</Button>
+                </Link>
+              </div>
+            </div>
+            <div className={clsx('row', styles.priceRow)}>
+              test</div>
+            <div className={clsx('row', styles.buttonsRow)}>
+              test</div>
+            <div className={clsx('row', styles.overviewRow)}>
+              test</div>
+            <div className={clsx('row', styles.infoRow)}>
+              test</div>
+            <div className={clsx('row', styles.socialRow)}>
+              test</div>
+          </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
